cb418137d274b82987b8ee7ff4a111ac8c385d06
xmateus-dev <bahamatteo@gmail.com>
2026-04-19 20:58:42 +0200

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const Database = require('better-sqlite3');
 
-const dataDir = path.join(__dirname, '../../data');
+const dataDir = path.join(process.cwd(), 'data');
 
 if (!fs.existsSync(dataDir)) {
   fs.mkdirSync(dataDir, { recursive: true });
